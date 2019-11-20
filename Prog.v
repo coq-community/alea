e5f0c00bc813fdf4681229d37d83ba16d57ef693
Vladimir Gladstein <vovaglad00@gmail.com>
2019-11-20 23:46:43 +0300

    bugs fixed

--- Prog.v
+++ Prog.v
@@ -934,15 +934,15 @@ Hint Resolve Imu_monotonic Imu_stable_eq.
 Lemma Imu_singl : forall (A:Type) (e:Distr A) (f:A->U),
            Ieq (Imu e (fun x => singl (f x))) (singl (mu e f)).
 unfold Ieq,Imu,singl; simpl; intuition.
-apply (mu_stable_eq e); simpl; apply ford_eq_intro; intro x; auto.
-apply (mu_stable_eq e); simpl; apply ford_eq_intro; intro x; auto.
+(*apply (mu_stable_eq e); simpl; apply ford_eq_intro; intro x; auto.
+apply (mu_stable_eq e); simpl; apply ford_eq_intro; intro x; auto.*)
 Qed.
 
 Lemma Imu_inf : forall (A:Type) (e:Distr A) (f:A->U),
            Ieq (Imu e (fun x => inf (f x))) (inf (mu e f)).
 unfold Ieq,Imu,inf; simpl; intuition.
 exact (mu_zero e).
-apply (mu_stable_eq e); simpl; apply ford_eq_intro; intro x; auto.
+(*apply (mu_stable_eq e); simpl; apply ford_eq_intro; intro x; auto.*)
 Qed.
 
 Lemma Imu_sup : forall (A:Type) (e:Distr A) (f:A->U),
